f294829bbeb1bf111ea60dd993faed5e25d94d53
roockk <31706114+roockk@users.noreply.github.com>
2017-09-19 13:08:47 -0700

    input>10------input>0

--- src/assignment2/Digits.java
+++ src/assignment2/Digits.java
@@ -1,15 +1,19 @@
 package assignment2;
 class addDigits {
+    public static void main(String[] args) {
+        addDigits test = new addDigits();
+        System.out.println(test.addDigits(1561));
 
+    }
     public int addDigits(int input) {
         int i;
         int result;
         i = 0;
         result = 0;
-        while (input >= 10) {
+        while (input > 0) {
             i = input % 10;
             result += i;
-            input /= input / 10;
+            input = input / 10;
         }
         result = (result < 10)? result : addDigits(result);
         return result;
